771e9c6abfa0b504967eaf7cabe15ae213be3dd7
adivianahd <adivianahd07@gmail.com>
2019-12-13 15:44:55 -0300

    say letter

--- src/games/listening.js
+++ src/games/listening.js
@@ -12,20 +12,28 @@ speech.init({
     'splitSentences': true
 })
 
-function  saludarAstrid() {
-    speech.speak({ text: 'Hola Astrid ?' });
+function getRandomLetter() {
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "CH"]
+    let position = Math.floor(letters.length * Math.random())
+
+    return letters[position]
+}
+
+function sayLetter(text) {
+    speech.speak({ text });
 }
 
 function listening() {
+    const letter = getRandomLetter();
+
 
     return (
         <div>
             <h1>Mi Componente</h1>
-            <button onClick={saludarAstrid}>Click me</button>
+            <button onClick={() => sayLetter(letter)}>Click me</button>
+
         </div>
     );
 }
 
 export default listening;
-
-//speech.speak({ text: 'Hola Astrid ?' });
